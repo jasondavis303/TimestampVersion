496eba8139bc4341053d4a84875ba5cdeb2ebd02
Jason Davis <jasondavis303@gmail.com>
2021-04-12 13:04:17 -0600

    Improve command line options

--- TimestampVersion/CLOptions.cs
+++ TimestampVersion/CLOptions.cs
@@ -1,13 +1,20 @@
 ﻿using CommandLine;
+using System.Collections.Generic;
 
 namespace TimestampVersion
 {
     class CLOptions
     {
-        [Option("xml-file", HelpText = "Xml file to create or update")]
-        public string XmlFile { get; set; }
+        [Option("xml-files", HelpText = "Xml file to create or update")]
+        public IEnumerable<string> XmlFiles { get; set; }
 
         [Option("env-file", HelpText = "Github Actions environment file to create or update")]
         public string EnvironmentFile { get; set; }
+
+        [Option("set-ov", HelpText ="Set the output variable TIMESTAMP_VERSION in Github Actions")]
+        public bool SetGAE { get; set; }
+
+        [Option("verbose")]
+        public bool Verbose { get; set; }
     }
 }
--- TimestampVersion/Program.cs
+++ TimestampVersion/Program.cs
@@ -34,40 +34,52 @@ namespace TimestampVersion
         {
             var version = Generator.Generate();
 
-            Console.WriteLine($"TIMESTAMP_VERSION={version}");
+            if(opts.Verbose)
+                Console.WriteLine($"Generated TIMESTAMP_VERSION={version}");
 
-
+            if(opts.SetGAE)
+            {
+                if (opts.Verbose)
+                    Console.WriteLine("Setting output variable");
+                Console.Out.WriteLine($"::set-output name=TIMESTAMP_VERSION::{version}");
+            }
+            
             if (!string.IsNullOrWhiteSpace(opts.EnvironmentFile))
             {
-                Console.WriteLine("Saving to: {0}", opts.EnvironmentFile);
+                if(opts.Verbose)
+                    Console.WriteLine("Saving to: {0}", opts.EnvironmentFile);
                 try { Directory.CreateDirectory(Path.GetDirectoryName(opts.EnvironmentFile)); }
                 catch { }
                 File.AppendAllLines(opts.EnvironmentFile, new string[] { $"TIMESTAMP_VERSION={version}" });
             }
-        
-            if(!string.IsNullOrWhiteSpace(opts.XmlFile))
+
+            foreach (var xmlFile in opts.XmlFiles)
             {
-                try { Directory.CreateDirectory(Path.GetDirectoryName(opts.XmlFile)); }
+                try { Directory.CreateDirectory(Path.GetDirectoryName(xmlFile)); }
                 catch { }
 
-                if (File.Exists(opts.XmlFile))
+                if (File.Exists(xmlFile))
                 {
-                    var doc = XDocument.Load(opts.XmlFile);
+                    var doc = XDocument.Load(xmlFile);
                     try
                     {
                         var node = doc.Root.Descendants("Version").First();
                         node.SetValue(version);
                     }
-                    catch 
+                    catch
                     {
                         throw new Exception("Version not found in existing xml file");
                     }
-                    Console.WriteLine("Saving to: {0}", opts.XmlFile);
-                    doc.Save(opts.XmlFile);
+
+                    if (opts.Verbose)
+                        Console.WriteLine("Saving to: {0}", xmlFile);
+                    doc.Save(xmlFile);
                 }
                 else
                 {
-                    File.WriteAllText(opts.XmlFile, $"<Version>{version}</Version>"); 
+                    if (opts.Verbose)
+                        Console.WriteLine("Saving to: {0}", xmlFile);
+                    File.WriteAllText(xmlFile, $"<Version>{version}</Version>");
                 }
             }
         }
