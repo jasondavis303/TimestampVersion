5fc05e3274961f47eecebc9eba1a57272edb926f
Jason Davis <jasondavis303@gmail.com>
2021-04-12 13:28:31 -0600

    Add better descriptions to CLOptions

--- TimestampVersion/CLOptions.cs
+++ TimestampVersion/CLOptions.cs
@@ -8,10 +8,10 @@ namespace TimestampVersion
         [Option("xml-files", HelpText = "Xml file to create or update")]
         public IEnumerable<string> XmlFiles { get; set; }
 
-        [Option("env-file", HelpText = "Github Actions environment file to create or update")]
+        [Option("env-file", HelpText = "Github Actions environment file to create or update (only works on Ubuntu)")]
         public string EnvironmentFile { get; set; }
 
-        [Option("set-ov", HelpText ="Set the output variable TIMESTAMP_VERSION in Github Actions")]
+        [Option("set-ov", HelpText ="Set the output variable TIMESTAMP_VERSION in Github Actions (works on Ubuntu and Windows)")]
         public bool SetGAE { get; set; }
 
         [Option("verbose")]
