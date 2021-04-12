881d76fbe365c511fb9301c1a8f01ef4a70dfff6
Jason Davis <jasondavis303@gmail.com>
2021-04-12 10:17:00 -0600

    Add project files.

---
+++ TimestampVersion/CLOptions.cs
@@ -0,0 +1,13 @@
+﻿using CommandLine;
+
+namespace TimestampVersion
+{
+    class CLOptions
+    {
+        [Option("xml-file", HelpText = "Xml file to create or update")]
+        public string XmlFile { get; set; }
+
+        [Option("env-file", HelpText = "Github Actions environment file to create or update")]
+        public string EnvironmentFile { get; set; }
+    }
+}
---
+++ TimestampVersion/Program.cs
@@ -0,0 +1,75 @@
+﻿using CommandLine;
+using System;
+using System.IO;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace TimestampVersion
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            int ret = 0;
+            
+            try
+            {
+                Parser.Default.ParseArguments<CLOptions>(args)
+                    .WithParsed(opts => Run(opts))
+                    .WithNotParsed(opts => ret = -1);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine();
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(ex.Message);
+                Console.ResetColor();
+                Console.WriteLine();
+            }
+
+            return ret;
+        }
+
+        static void Run(CLOptions opts)
+        {
+            var dt = DateTime.UtcNow;
+            var version = new Version
+                (
+                    dt.Year - 2000,
+                    dt.Month,
+                    dt.Day,
+                    (dt.Hour * 60) + dt.Minute
+                ).ToString();
+
+            Console.WriteLine($"TIMESTAMP_VERSION={version}");
+
+
+            if (!string.IsNullOrWhiteSpace(opts.EnvironmentFile))
+                File.AppendAllLines(opts.EnvironmentFile, new string[] { $"TIMESTAMP_VERSION={version}" });
+        
+            if(!string.IsNullOrWhiteSpace(opts.XmlFile))
+            {
+                Directory.CreateDirectory(Path.GetDirectoryName(opts.XmlFile));
+
+                if (File.Exists(opts.XmlFile))
+                {
+                    var doc = XDocument.Load(opts.XmlFile);
+                    try
+                    {
+                        var node = doc.Root.Descendants("Version").First();
+                        node.SetValue(version);
+                    }
+                    catch 
+                    {
+                        throw new Exception("Version not found in existing xml file");
+                    }
+                    doc.Save(opts.XmlFile);
+                }
+                else
+                {
+                    File.WriteAllText(opts.XmlFile, $"<Version>{version}</Version>"); 
+                }
+            }
+        }
+    }
+}
