b37194c2b2faa714b5990e7ca2141da08c4aa433
jasondavis303 <jasondavis303@gmail.com>
2021-11-24 11:40:26 -0700

    Make sure MSIX version ends with 0

--- TimestampVersion/Generator.cs
+++ TimestampVersion/Generator.cs
@@ -8,7 +8,7 @@ namespace TimestampVersion
 
         public static Version Generate() => Generate(DateTime.UtcNow);
 
-        public static Version GenerateMSIX(DateTime dateTime) => new Version(dateTime.Year, dateTime.DayOfYear, (dateTime.Hour * 60) + dateTime.Minute);
+        public static Version GenerateMSIX(DateTime dateTime) => new Version(dateTime.Year, dateTime.DayOfYear, (dateTime.Hour * 60) + dateTime.Minute, 0);
 
         public static Version GenerateMSIX() => GenerateMSIX(DateTime.UtcNow);
     }
